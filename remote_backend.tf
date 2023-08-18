2e6b1e7afee090bc1b372e5a189ef07ab3e392b2
jakemalmad <jmalmad@amazon.com>
2023-08-18 19:50:49 +0000

    add second tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "jmalmad-amzn"
     workspaces {
       name = "hashicat-aws"
